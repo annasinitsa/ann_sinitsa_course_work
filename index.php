e9507d66042dc33064d697e24d512f628fe7df62
Ann Sinitsa <annasinitsa20@gmail.com>
2019-12-11 01:27:01 +0200

    add composer

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/mustache/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+$m = new Mustache_Engine;
+echo $m->render('Hello {{planet}}', array('planet' => 'World!'));
